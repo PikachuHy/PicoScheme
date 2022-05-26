40eb1de3a88f3dd959373ed8e4e42b767d1125d3
PikachuHy <pikachuhy@163.com>
2022-05-26 22:43:41 +0800

    update test.scm

--- test/test.scm
+++ test/test.scm
@@ -2,38 +2,30 @@
 (define *tests-run* 0)
 (define *tests-passed* 0)
 (define primitive-add +)
-(define-syntax test
-  (syntax-rules ()
-    ((test name expect expr)
-     (test expect expr))
-    ((test expect expr)
-     (begin
-       (set! *tests-run* (+ *tests-run* 1))
-       (let ((str (call-with-output-string
-                    (lambda (out)
-                      (display *tests-run* out)
-                      (display ". " out)
-                      (display 'expr out))))
-             (res expr))
-         (display str)
-         (write-char #\space)
-         (display (make-string (max 0 (- 150 (string-length str))) #\.))
-         (flush-output)
-         (cond
-          ((equal? res expect)
-           (set! *tests-passed* (primitive-add *tests-passed* 1))
-           (display-green " [PASS]\n")
-           #t)
-          (else
-           (display-red " [FAIL]\n")
-           (display "    expected ") (write expect)
-           (display " but got ") (write res) (newline)
-           #f)
-           ))))))
-
-(define-syntax test-assert
-  (syntax-rules ()
-    ((test-assert expr) (test #t expr))))
+(define-macro (test expect expr)
+  `(begin
+     (set! *tests-run* (+ *tests-run* 1))
+     (let (
+	   (str (call-with-output-string
+		 (lambda (out)
+		   (display *tests-run* out)
+		   (display ". " out)
+		   (display ',expr out))))
+	   (res ,expr))
+       (display str)
+       (write-char #\space)
+       (display (make-string (max 0 (- 150 (string-length str))) #\.))
+       (flush-output)
+       (cond
+	((equal? res ,expect)
+	 (set! *tests-passed* (+ *tests-passed* 1))
+	 (display-green " [PASS]\n")
+	 #t)
+	(else
+	 (display-red " [FAIL]\n")
+	 (display "    expected ") (write ,expect)
+	 (display " but got ") (write res) (newline)
+	 #f)))))
 
 (define (test-begin . name)
   #f)
@@ -46,3 +38,5 @@
   (write (* (/ *tests-passed* *tests-run*) 100))
   (display "%)")
   (newline))
+
+(export '(test-begin test-end))
